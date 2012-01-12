e1e89bfd397484e5c0a6a28b8a03895bf23b6ff4
Rahul <lethalbrains@gmail.com>
2012-01-12 13:49:13 +0530

    Initital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aliens::Application.config.secret_token = '4e81229b1799df96c1516a61e91e811032d9f18d6b0bbebeefeb5e35279769963e36f7ec95c8c0bae87f84665caeb9cbd029b55274cd1afe67ba6f4018d2dd6f'
